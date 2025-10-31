3ed9ffe3c425e62d4df8636d16f4a61c0a36443f
Gurvinder Singh <avinashkaur842@gmail.com>
2025-11-01 01:23:54 +0530

    settings work

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -1,19 +1,25 @@
 import { getAllWebviewWindows } from "@tauri-apps/api/webviewWindow";
 import { useEffect, useState } from "react";
+import { disable, enable, isEnabled } from "@tauri-apps/plugin-autostart";
 
 export default function Settings() {
   const [alwaysOnTop, setAlwaysOnTop] = useState(false);
+  const [startOnLogin, setStartOnLogin] = useState(false);
 
   useEffect(() => {
     (async () => {
       const windows = await getAllWebviewWindows();
+
       // Find the main window (not the settings window)
       const mainWindow = windows.find((w) => w.label == "main");
-      console.log(mainWindow);
       if (mainWindow) {
         const isOnTop = await mainWindow.isAlwaysOnTop();
         setAlwaysOnTop(isOnTop);
       }
+
+      const isAutoStart = await isEnabled();
+
+      setStartOnLogin(isAutoStart);
     })();
   }, []);
 
@@ -24,6 +30,17 @@ export default function Settings() {
     // Apply to all windows
     const windows = await getAllWebviewWindows();
     await Promise.all(windows.map((window) => window.setAlwaysOnTop(newValue)));
+  };
+
+  const toggleAutoStart = async () => {
+    const newValue = !startOnLogin;
+    setStartOnLogin(newValue);
+
+    if (newValue) {
+      await enable();
+    } else {
+      await disable();
+    }
   };
 
   return (
@@ -41,7 +58,11 @@ export default function Settings() {
         </div>
         <div className="flex items-center justify-between">
           <span>Start on login</span>
-          <input type="checkbox" className="" />
+          <input
+            type="checkbox"
+            checked={startOnLogin}
+            onChange={toggleAutoStart}
+          />
         </div>
       </div>
       <p className="mt-4 text-xs text-neutral-400">
