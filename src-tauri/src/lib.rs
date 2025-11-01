ed723370631455973cd15ac932425ecd4701e635
Gurvinder Singh <avinashkaur842@gmail.com>
2025-11-01 06:09:15 +0530

    Update dependencies for macOS support by adding `cocoa` and `objc`, and enhance window behavior in the Tauri application. Remove unused icon configuration in `tauri.conf.json`.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri::Manager;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -6,7 +8,29 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    tauri::Builder::default().setup(|app| {
+        let main_window = app.get_webview_window("main").unwrap();
+
+        #[cfg(target_os = "macos")]
+        {
+            use cocoa::appkit::NSWindow;
+            use cocoa::base::id;
+            unsafe {
+                use cocoa::base::NO;
+
+                let ns_window = main_window.ns_window().unwrap() as id;
+                ns_window.setCollectionBehavior_(
+                    cocoa::appkit::NSWindowCollectionBehavior::NSWindowCollectionBehaviorCanJoinAllSpaces
+                        | cocoa::appkit::NSWindowCollectionBehavior::NSWindowCollectionBehaviorFullScreenAuxiliary,
+                );
+                ns_window.setLevel_((cocoa::appkit::NSMainMenuWindowLevel + 2).into()); // float above normal windows
+                ns_window.setHidesOnDeactivate_(NO); // don’t hide when switching apps
+            }
+
+            Ok(())
+        }
+
+    })
         .plugin(tauri_plugin_autostart::Builder::new().build())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_opener::init())
